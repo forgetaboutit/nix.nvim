2ebd6f5e6d5e539cd4eec4a23ba0f489be708136
Samuel Schuhmacher <post@samuel-schuhmacher.de>
2025-02-22 16:49:29 +0100

    Change highlighted column to 80

--- nvim/init.lua
+++ nvim/init.lua
@@ -94,11 +94,10 @@ vim.diagnostic.config {
 
 g.editorconfig = true
 
-vim.opt.colorcolumn = '100'
+vim.opt.colorcolumn = '80'
 
 -- Native plugins
 cmd.filetype('plugin', 'indent', 'on')
 cmd.packadd('cfilter') -- Allows filtering the quickfix list with :cfdo
-
 -- let sqlite.lua (which some plugins depend on) know where to find sqlite
 vim.g.sqlite_clib_path = require('luv').os_getenv('LIBSQLITE')
